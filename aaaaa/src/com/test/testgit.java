18aafa14b89cf233aeb188eedc16977009740a08
liganghe <liganghe@weiyankeji.cn>
2017-10-31 17:00:55 +0800

    ceshi

--- aaaaa/src/com/test/testgit.java
+++ aaaaa/src/com/test/testgit.java
@@ -5,7 +5,7 @@ public class testgit {
 				// TODO Auto-generated method stub
 					public static void main(String[] args) {
 						System.out.print("hello");
-					
+//					//dwadawdawdawd
 					}
 					
 						
